d37ba46b100ec8501027d96b2794cb4611bf8d6e
peTheProgrammer <peTheProgrammer@gmail.com>
2021-07-27 20:48:12 +0200

    Added a logger which gets created and prints a message if cl hasn't been initialized before calling CL_CREATE_LOGGER.

--- c_log/c_log.c
+++ c_log/c_log.c
@@ -76,139 +76,148 @@ void _cl_logger_log(CL_Logger *logger, uint32_t lvl, const char *file, uint32_t 
 CL_Logger *_cl_logger_create(uint16_t ouput_count, const char *name, const char *pattern)
 {
 	if (g_cl_info == NULL)
-		_cl_init();
-
-	CL_Logger *logger = (CL_Logger *)malloc(sizeof(*logger));
-	logger->log_level = CL_LOG_LEVEL_TRACE;
-	logger->output_count_c = 0;
-	if(ouput_count == 0)
-		ouput_count = 2;
-	logger->output_count_m = ouput_count;
-	logger->outputs = malloc(sizeof(*logger->outputs) * ouput_count);
-	logger->output_colors = malloc(sizeof(*logger->output_colors) * ouput_count);
-	if(name == NULL)
-		name = "UNNAMED";
-	logger->name = name;
-
-	//LOG_INFO("Logger %s initialized succesfully.", name);
-
-	if (pattern == NULL)
 	{
-		pattern = g_cl_info->default_pattern;
+		_cl_init(CL_DEFAULT_PATTERN);
+		CL_Logger *logger_default = _cl_logger_create(1, "DEFAULT", CL_DEFAULT_PATTERN);
+		_cl_logger_output_add(logger_default, stdout, CL_COLOR_ENABLED);
+		CL_LOG_ERROR(logger_default, "Could not create logger. The library should be initialized using 'CL_INIT()' before creating a logger.");
+		_cl_logger_destroy(logger_default);
+		_cl_terminate();
 	}
-
-	logger->pattern.string = pattern;
-	logger->pattern.segment_count_c = 0;
-	logger->pattern.segment_count_m = 16;
-	logger->pattern.segment_types = malloc(sizeof(*logger->pattern.segment_types) * logger->pattern.segment_count_m);
-	logger->pattern.segment_values = malloc(sizeof(*logger->pattern.segment_values) * logger->pattern.segment_count_m);
-
-	// compile pattern
+	else
 	{
-		uint32_t pattern_length = strlen(pattern);
-		const char *segment_start = pattern;
-		for (uint32_t pattern_index = 0; pattern_index < pattern_length; pattern_index++)
+		CL_Logger *logger = (CL_Logger *)malloc(sizeof(*logger));
+		logger->log_level = CL_LOG_LEVEL_TRACE;
+		logger->output_count_c = 0;
+		if (ouput_count == 0)
+			ouput_count = 2;
+		logger->output_count_m = ouput_count;
+		logger->outputs = malloc(sizeof(*logger->outputs) * ouput_count);
+		logger->output_colors = malloc(sizeof(*logger->output_colors) * ouput_count);
+		if (name == NULL)
+			name = "UNNAMED";
+		logger->name = name;
+
+		//LOG_INFO("Logger %s initialized succesfully.", name);
+
+		if (pattern == NULL)
 		{
-			if (pattern[pattern_index] == '%')
+			pattern = g_cl_info->default_pattern;
+		}
+
+		logger->pattern.string = pattern;
+		logger->pattern.segment_count_c = 0;
+		logger->pattern.segment_count_m = 16;
+		logger->pattern.segment_types = malloc(sizeof(*logger->pattern.segment_types) * logger->pattern.segment_count_m);
+		logger->pattern.segment_values = malloc(sizeof(*logger->pattern.segment_values) * logger->pattern.segment_count_m);
+
+		// compile pattern
+		{
+			uint32_t pattern_length = strlen(pattern);
+			const char *segment_start = pattern;
+			for (uint32_t pattern_index = 0; pattern_index < pattern_length; pattern_index++)
 			{
-				if (logger->pattern.segment_count_c + 2 >= logger->pattern.segment_count_m)
+				if (pattern[pattern_index] == '%')
+				{
+					if (logger->pattern.segment_count_c + 2 >= logger->pattern.segment_count_m)
+					{
+						logger->pattern.segment_count_m *= 2;
+						logger->pattern.segment_types = realloc(logger->pattern.segment_types, sizeof(*logger->pattern.segment_types) * logger->pattern.segment_count_m);
+						logger->pattern.segment_values = realloc(logger->pattern.segment_values, sizeof(*logger->pattern.segment_values) * logger->pattern.segment_count_m);
+					}
+					// push string
+					{
+						const char *segment_end = pattern + pattern_index;
+						uint32_t segment_length = segment_end - segment_start;
+						if (segment_length != 0)
+						{
+							char *string = malloc(sizeof(*string) * (segment_length + 1));
+							for (uint32_t string_index = 0; string_index < segment_length; string_index++)
+							{
+								string[string_index] = segment_start[string_index];
+							}
+							string[segment_length] = '\0';
+							logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_STRING;
+							logger->pattern.segment_values[logger->pattern.segment_count_c] = string;
+							logger->pattern.segment_count_c++;
+						}
+					}
+
+					pattern_index++;
+					switch (pattern[pattern_index])
+					{
+					case 'F':
+						logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_FILE;
+						break;
+					case 'L':
+						logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_LINE;
+						break;
+					case 'V':
+						logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_LOG_LEVEL;
+						break;
+					case 'T':
+						logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_TIME;
+						break;
+					case 'M':
+						logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_MESSAGE;
+						break;
+					case 'N':
+						logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_NAME;
+						break;
+					case 'C':
+						logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_COLOR;
+						break;
+					}
+					if (pattern[pattern_index] == '%')
+					{
+						char *string = malloc(sizeof(*string) * 2);
+						string[0] = '%';
+						string[1] = '\0';
+						logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_STRING;
+						logger->pattern.segment_values[logger->pattern.segment_count_c] = string;
+					}
+					else
+					{
+						logger->pattern.segment_values[logger->pattern.segment_count_c] = NULL;
+					}
+					logger->pattern.segment_count_c++;
+
+					segment_start = pattern + pattern_index + 1;
+				}
+			}
+			const char *segment_end = pattern + pattern_length + 1;
+			uint32_t segment_length = segment_end - segment_start;
+			if (segment_length != 0)
+			{
+				if (logger->pattern.segment_count_c + 1 >= logger->pattern.segment_count_m)
 				{
 					logger->pattern.segment_count_m *= 2;
 					logger->pattern.segment_types = realloc(logger->pattern.segment_types, sizeof(*logger->pattern.segment_types) * logger->pattern.segment_count_m);
 					logger->pattern.segment_values = realloc(logger->pattern.segment_values, sizeof(*logger->pattern.segment_values) * logger->pattern.segment_count_m);
 				}
-				// push string
+				char *string = malloc(sizeof(*string) * (segment_length + 1));
+				for (uint32_t string_index = 0; string_index < segment_length; string_index++)
 				{
-					const char *segment_end = pattern + pattern_index;
-					uint32_t segment_length = segment_end - segment_start;
-					if (segment_length != 0)
-					{
-						char *string = malloc(sizeof(*string) * (segment_length + 1));
-						for (uint32_t string_index = 0; string_index < segment_length; string_index++)
-						{
-							string[string_index] = segment_start[string_index];
-						}
-						string[segment_length] = '\0';
-						logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_STRING;
-						logger->pattern.segment_values[logger->pattern.segment_count_c] = string;
-						logger->pattern.segment_count_c++;
-					}
+					string[string_index] = segment_start[string_index];
 				}
-
-				pattern_index++;
-				switch (pattern[pattern_index])
-				{
-				case 'F':
-					logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_FILE;
-					break;
-				case 'L':
-					logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_LINE;
-					break;
-				case 'V':
-					logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_LOG_LEVEL;
-					break;
-				case 'T':
-					logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_TIME;
-					break;
-				case 'M':
-					logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_MESSAGE;
-					break;
-				case 'N':
-					logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_NAME;
-					break;
-				case 'C':
-					logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_COLOR;
-					break;
-				}
-				if (pattern[pattern_index] == '%')
-				{
-					char *string = malloc(sizeof(*string) * 2);
-					string[0] = '%';
-					string[1] = '\0';
-					logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_STRING;
-					logger->pattern.segment_values[logger->pattern.segment_count_c] = string;
-				}
-				else
-				{
-					logger->pattern.segment_values[logger->pattern.segment_count_c] = NULL;
-				}
+				string[segment_length] = '\0';
+				logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_STRING;
+				logger->pattern.segment_values[logger->pattern.segment_count_c] = string;
 				logger->pattern.segment_count_c++;
+			}
+		}
 
-				segment_start = pattern + pattern_index + 1;
-			}
-		}
-		const char *segment_end = pattern + pattern_length + 1;
-		uint32_t segment_length = segment_end - segment_start;
-		if (segment_length != 0)
-		{
-			if (logger->pattern.segment_count_c + 1 >= logger->pattern.segment_count_m)
-			{
-				logger->pattern.segment_count_m *= 2;
-				logger->pattern.segment_types = realloc(logger->pattern.segment_types, sizeof(*logger->pattern.segment_types) * logger->pattern.segment_count_m);
-				logger->pattern.segment_values = realloc(logger->pattern.segment_values, sizeof(*logger->pattern.segment_values) * logger->pattern.segment_count_m);
-			}
-			char *string = malloc(sizeof(*string) * (segment_length + 1));
-			for (uint32_t string_index = 0; string_index < segment_length; string_index++)
-			{
-				string[string_index] = segment_start[string_index];
-			}
-			string[segment_length] = '\0';
-			logger->pattern.segment_types[logger->pattern.segment_count_c] = CL_SEGMENT_TYPE_STRING;
-			logger->pattern.segment_values[logger->pattern.segment_count_c] = string;
-			logger->pattern.segment_count_c++;
-		}
+		return logger;
 	}
-
-	return logger;
 }
 
 void _cl_logger_output_add(CL_Logger *logger, FILE *file, uint8_t color_output)
 {
-	if(logger->output_count_c == logger->output_count_m)
+	if (logger->output_count_c == logger->output_count_m)
 	{
 		logger->output_count_m *= 2;
-		logger->outputs = realloc(logger->outputs, sizeof(*logger->outputs) *  logger->output_count_m);
-		logger->output_colors = realloc(logger->output_colors, sizeof(*logger->output_colors) *  logger->output_count_m);
+		logger->outputs = realloc(logger->outputs, sizeof(*logger->outputs) * logger->output_count_m);
+		logger->output_colors = realloc(logger->output_colors, sizeof(*logger->output_colors) * logger->output_count_m);
 	}
 	logger->outputs[logger->output_count_c] = file;
 	logger->output_colors[logger->output_count_c] = color_output;
@@ -249,7 +258,10 @@ void _cl_init(const char *default_pattern)
 void _cl_terminate()
 {
 	if (g_cl_info != NULL)
+	{
 		free(g_cl_info);
+		g_cl_info = NULL;
+	}
 }
 
 void _log_level_set(CL_Logger *logger, uint32_t lvl)
